26a021ec16bb19fc9b06298f9cdf704721326924
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2020-05-29 11:52:44 +0200

    Reproduces of DemoDataGenerator startup event not being called

---
+++ src/main/java/org/acme/bootstrap/DemoDataGenerator.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.acme.bootstrap;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.transaction.Transactional;
+
+import io.quarkus.runtime.StartupEvent;
+import org.acme.domain.Lesson;
+import org.acme.domain.Room;
+import org.acme.domain.Timeslot;
+
+@ApplicationScoped
+public class DemoDataGenerator {
+
+    @Transactional
+    public void generateData(@Observes StartupEvent startupEvent) {
+// If you don't see this line, it reproduces
+System.out.println("THE STARTUP EVENT HAPPENED <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+        List<Timeslot> timeslotList = new ArrayList<>(10);
+        timeslotList.add(new Timeslot(DayOfWeek.MONDAY, LocalTime.of(8, 30), LocalTime.of(9, 30)));
+        timeslotList.add(new Timeslot(DayOfWeek.MONDAY, LocalTime.of(9, 30), LocalTime.of(10, 30)));
+        timeslotList.add(new Timeslot(DayOfWeek.MONDAY, LocalTime.of(10, 30), LocalTime.of(11, 30)));
+        timeslotList.add(new Timeslot(DayOfWeek.MONDAY, LocalTime.of(13, 30), LocalTime.of(14, 30)));
+        timeslotList.add(new Timeslot(DayOfWeek.MONDAY, LocalTime.of(14, 30), LocalTime.of(15, 30)));
+
+        timeslotList.add(new Timeslot(DayOfWeek.TUESDAY, LocalTime.of(8, 30), LocalTime.of(9, 30)));
+        timeslotList.add(new Timeslot(DayOfWeek.TUESDAY, LocalTime.of(9, 30), LocalTime.of(10, 30)));
+        timeslotList.add(new Timeslot(DayOfWeek.TUESDAY, LocalTime.of(10, 30), LocalTime.of(11, 30)));
+        timeslotList.add(new Timeslot(DayOfWeek.TUESDAY, LocalTime.of(13, 30), LocalTime.of(14, 30)));
+        timeslotList.add(new Timeslot(DayOfWeek.TUESDAY, LocalTime.of(14, 30), LocalTime.of(15, 30)));
+        Timeslot.persist(timeslotList);
+
+        List<Room> roomList = new ArrayList<>(3);
+        roomList.add(new Room("Room A"));
+        roomList.add(new Room("Room B"));
+        roomList.add(new Room("Room C"));
+        Room.persist(roomList);
+
+        List<Lesson> lessonList = new ArrayList<>();
+        lessonList.add(new Lesson("Math", "A. Turing", "9th grade"));
+        lessonList.add(new Lesson("Math", "A. Turing", "9th grade"));
+        lessonList.add(new Lesson("Physics", "M. Curie", "9th grade"));
+        lessonList.add(new Lesson("Chemistry", "M. Curie", "9th grade"));
+        lessonList.add(new Lesson("Biology", "C. Darwin", "9th grade"));
+        lessonList.add(new Lesson("History", "I. Jones", "9th grade"));
+        lessonList.add(new Lesson("English", "I. Jones", "9th grade"));
+        lessonList.add(new Lesson("English", "I. Jones", "9th grade"));
+        lessonList.add(new Lesson("Spanish", "P. Cruz", "9th grade"));
+        lessonList.add(new Lesson("Spanish", "P. Cruz", "9th grade"));
+        lessonList.add(new Lesson("Math", "A. Turing", "10th grade"));
+        lessonList.add(new Lesson("Math", "A. Turing", "10th grade"));
+        lessonList.add(new Lesson("Math", "A. Turing", "10th grade"));
+        lessonList.add(new Lesson("Physics", "M. Curie", "10th grade"));
+        lessonList.add(new Lesson("Chemistry", "M. Curie", "10th grade"));
+        lessonList.add(new Lesson("French", "M. Curie", "10th grade"));
+        lessonList.add(new Lesson("Geography", "C. Darwin", "10th grade"));
+        lessonList.add(new Lesson("History", "I. Jones", "10th grade"));
+        lessonList.add(new Lesson("English", "P. Cruz", "10th grade"));
+        lessonList.add(new Lesson("Spanish", "P. Cruz", "10th grade"));
+        Lesson.persist(lessonList);
+    }
+
+}
---
+++ src/main/java/org/acme/domain/Timeslot.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.acme.domain;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+@Entity
+public class Timeslot extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public Timeslot() {
+    }
+
+    public Timeslot(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+}
